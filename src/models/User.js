ef5f5008e65800b369ea67a413a8eec80e3d49bf
Rogério Bonfim <rogerio.bonfim@auaha.com.br>
2020-01-16 10:22:14 -0300

    correct migrations and users & tests

--- src/models/User.js
+++ src/models/User.js
@@ -1,50 +1,45 @@
-const { DataTypes, Model } = require("sequelize");
+const { DataTypes, Model } = require('sequelize');
 
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 class User extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                client_id: DataTypes.STRING,
-                name: DataTypes.STRING,
-                email: DataTypes.STRING,
-                password: DataTypes.VIRTUAL,
-                password_hash: DataTypes.STRING,
-                auth: DataTypes.STRING,
-                credentials: DataTypes.STRING
-            },
-            {
-                hooks: {
-                    beforeSave: async (user) => {
-                        if (user.password) {
-                            user.password_hash = await bcrypt.hash(
-                                user.password,
-                                8
-                            );
-                        }
-                    }
-                },
-                sequelize
-            }
-        );
-    }
+	static init(sequelize) {
+		super.init(
+			{
+				name: DataTypes.STRING,
+				email: DataTypes.STRING,
+				password: DataTypes.VIRTUAL,
+				password_hash: DataTypes.STRING,
+				type: DataTypes.STRING,
+			},
+			{
+				hooks: {
+					beforeSave: async user => {
+						if (user.password) {
+							user.password_hash = await bcrypt.hash(user.password, 8);
+						}
+					},
+				},
+				sequelize,
+			},
+		);
+	}
 
-    static associate(models) {
-        this.hasMany(models.Address, {
-            foreignKey: "user_id",
-            as: "addresses"
-        });
-    }
+	static associate(models) {
+		this.hasMany(models.Address, {
+			foreignKey: 'user_id',
+			as: 'addresses',
+		});
+	}
 }
 
 User.prototype.checkPassword = function(password) {
-    return bcrypt.compare(password, this.password_hash);
+	return bcrypt.compare(password, this.password_hash);
 };
 
 User.prototype.generateToken = function() {
-    return jwt.sign({ id: this.id }, process.env.APP_SECRET);
+	return jwt.sign({ id: this.id }, process.env.APP_SECRET);
 };
 
 module.exports = User;
