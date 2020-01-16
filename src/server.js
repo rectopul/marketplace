c6ef8863310f7676945e243711ffd112a6b71742
Rogério Bonfim <rogerio.bonfim@auaha.com.br>
2020-01-16 09:54:35 -0300

    app listem port heroku

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config({
-    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+require('dotenv').config({
+	path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
-const express = require("express");
-const routes = require("./routes");
+const express = require('express');
+const routes = require('./routes');
 
-require("./database");
+require('./database');
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use((req, res) => {
-    res.status(404).send({ url: req.originalUrl + " not found" });
+	res.status(404).send({ url: req.originalUrl + ' not found' });
 });
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
