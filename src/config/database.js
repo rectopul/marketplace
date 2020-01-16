f39f56ba5716f8065797d0dfd5ea9ad6b405ff96
Rogério Bonfim <rogeriomateus@icloud.com>
2020-01-15 22:50:47 -0300

    servers

--- src/config/database.js
+++ src/config/database.js
@@ -2,20 +2,45 @@ require("dotenv").config({
     path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
 
-module.exports = {
-    dialect: process.env.DB_DIALECT || "postgres",
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    storage: "./__tests__/database.sqlite",
-    logging: false,
-    options: {
-        port: 5432
-    },
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true
-    }
-};
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    database = {
+        dialect: process.env.DB_DIALECT || "postgres",
+        host: "ec2-174-129-253-175.compute-1.amazonaws.com",
+        username: "rvrxmvewbbncnc",
+        password:
+            "cb8750ed6e48b289d51972764d4939cf7f248b52684560a13d49feace9b9ba7c",
+        database: "dagec71e23cc8b",
+        storage: "./__tests__/database.sqlite",
+        logging: true, //false
+        options: {
+            port: 5432
+        },
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
+        }
+    };
+} else {
+    // the application is executed on the local machine ... use mysql
+    database = {
+        dialect: process.env.DB_DIALECT || "postgres",
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        storage: "./__tests__/database.sqlite",
+        logging: false,
+        options: {
+            port: 5432
+        },
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
+        }
+    };
+}
+
+module.exports = database;
