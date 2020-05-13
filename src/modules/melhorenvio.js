2b90c43744b4216afd430031296a1c8d6a9ab40f
Rogério Bonfim <rogeriomateus@icloud.com>
2020-05-13 15:12:33 -0300

    envio

--- src/modules/melhorenvio.js
+++ src/modules/melhorenvio.js
@@ -8,28 +8,41 @@ module.exports = {
             const { client_id, client_secret } = req.body
             const { code } = req.query
 
-            let bodyFormData = new FormData()
+            /* let bodyFormData = new FormData()
 
             bodyFormData.set('grant_type', 'authorization_code')
             bodyFormData.set('client_id', client_id)
             bodyFormData.set('client_secret', client_secret)
             bodyFormData.set('redirect_uri', callback)
-            bodyFormData.set('code', code)
+            bodyFormData.set('code', code) */
+
+            const parameters = {
+                grant_type: 'authorization_code',
+                client_id,
+                client_secret,
+                redirect_uri: callback,
+                code,
+            }
+
+            if (code) return res.json(code)
 
             const scopes = `shipping-calculate shipping-cancel shipping-checkout shipping-companies shipping-generate shipping-preview shipping-print shipping-share shipping-tracking ecommerce-shipping transactions-read users-read users-write webhooks-read webhooks-write`
 
             const urlEnvio = `${url}/oauth/authorize?client_id=${client_id}&redirect_uri=${callback}&response_type=code&scope=${scopes}`
 
-            const generateToken = await axios({
+            /* const generateToken = await axios({
                 method: 'post',
                 url: urlEnvio,
-                data: bodyFormData,
+                data: parameters,
                 headers: { 'Content-Type': 'application/json' },
-            })
+            }) */
 
-            return res.json(generateToken)
+            console.log(`Retorno do axios`, urlEnvio)
+
+            return res.json(urlEnvio)
         } catch (error) {
             console.log(error)
+            return res.status(400).send({ error })
         }
     },
 }
